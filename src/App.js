d1e61df2e2d8955579893c0654fec199e04390f1
Eric Jovian <43366082+ericjovian@users.noreply.github.com>
2022-07-23 19:10:34 +0700

    cleaning unused file and adding tsparticle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Particles from "react-tsparticles";
+import { loadFull } from "tsparticles";
+import "./styles.css";
 
-function App() {
+export default function App() {
+  const particlesInit = async (main) => {
+    console.log(main);
+
+    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
+    // this loads the tsparticles package bundle, it's the easiest method for getting everything ready
+    // starting from v2 you can add only the features you need reducing the bundle size
+    await loadFull(main);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Particles
+        options={{
+          background: {
+            color: "#000000"
+          },
+          fpsLimit: 60,
+          interactivity: {
+            detectsOn: "canvas",
+            events: {
+              resize: true
+            }
+          },
+          particles: {
+            color: {
+              value: "#ffffff"
+            },
+            number: {
+              density: {
+                enable: true,
+                area: 1000
+              },
+              limit: 0,
+              value: 300
+            },
+            opacity: {
+              animation: {
+                enable: true,
+                minimumValue: 0.05,
+                speed: 1,
+                sync: false
+              },
+              random: {
+                enable: true,
+                minimumValue: 0.05
+              },
+              value: 1
+            },
+            shape: {
+              type: "star"
+            },
+            size: {
+              randmon: {
+                enable: true,
+                minimumValue: 0.5,
+                value: 1
+              }
+            }
+          }
+        }}
+      />
     </div>
   );
 }
-
-export default App;
