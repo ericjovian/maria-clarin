82e34540c03a25c75184f47f74731bb3e76792c2
Eric Jovian <43366082+ericjovian@users.noreply.github.com>
2022-07-23 19:13:22 +0700

    adding init function for tsparticles

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ export default function App() {
   return (
     <div className="App">
       <Particles
+        init={particlesInit}
         options={{
           background: {
             color: "#000000"
